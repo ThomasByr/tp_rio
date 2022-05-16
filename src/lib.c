10c6dc86e10baab3e2b677a241c0bb5559bc0547
ThomasByr <tbouyer2000@gmail.com>
2022-05-16 21:29:35 +0200

    /src/lib : helper

--- src/lib.c
+++ src/lib.c
@@ -15,3 +15,39 @@ noreturn void panic(int syserr, const char *restrict fmt, ...) {
 
     exit(EXIT_FAILURE);
 }
+
+void debug(int first, const char *restrict fmt, ...) {
+#ifndef DEBUG
+    (void)first;
+    (void)fmt;
+#else
+    va_list ap;
+
+    va_start(ap, fmt);
+    if (first) {
+        fprintf(stdout, FG_GRN "  DEBUG: " RST);
+    }
+    vfprintf(stdout, fmt, ap);
+    va_end(ap);
+
+    int n = fflush(stdout);
+    if (n == EOF) {
+        panic(1, "fflush failure");
+    }
+#endif
+}
+
+void info(int first, const char *restrict fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    if (first) {
+        fprintf(stdout, FG_BLU "\n   INFO: " RST);
+    }
+    vfprintf(stdout, fmt, ap);
+    va_end(ap);
+    int n = fflush(stdout);
+    if (n == EOF) {
+        panic(1, "fflush failure");
+    }
+}
